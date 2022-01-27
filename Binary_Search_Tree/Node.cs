c29d1aaac4d95958e7f96d1d249e013d01bc9249
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-27 10:30:32 +0800

    Add project files.

---
+++ Binary_Search_Tree/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Search_Tree
+{
+    class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+
+        public int Data { get; set }
+    }
+}
