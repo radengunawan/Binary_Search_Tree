a1092b5912236aa3a05d9f215c2e8cdf0ff5dbbe
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-27 10:42:15 +0800

    semi-completed

---
+++ Binary_Search_Tree/BinaryTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Search_Tree
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add (int value)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+
+
+            }//END loop
+
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+                this.Root = newNode;
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+
+            }
+            return true;
+
+
+        } //END method Add
+
+
+
+
+    }// END class
+}
